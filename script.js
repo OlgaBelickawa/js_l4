f8e0837957f50703c605751640715c4d202f415a
OlgaBelickawa <145890221+OlgaBelickawa@users.noreply.github.com>
2023-12-19 14:39:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let pricePen = 500;
+let pricePencil = 700;
+let priceSketchbook = 1200;
+let priceColoredPencils = 800;
+let colorScetchbook = "Black";
+let colorPencil = "White";
+let colorPen = "Purple";
+let countColoredPencils = "12";
+
+console.log("Цена ручки:", String (pricePen), "сум");
+console.log("Цена карандаша:", String (pricePencil), "сум");
+console.log("Цена Альбома для рисования:", String (priceSketchbook), "сум");
+console.log("Цена цветных карандашей:", Number(priceColoredPencils), "сум");
+console.log("Цвет Альбома для рисования - Чёрный", Boolean(colorScetchbook));
+console.log("Цвет карандаша - Белый", Boolean(colorPencil));
+console.log("Цвет ручки - Фиолетовый", Boolean(colorPen));
+console.log("Количество цветных карандашей в упаковке - 12", Boolean(countColoredPencils));
